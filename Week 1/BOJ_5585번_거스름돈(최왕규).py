95b765883d41ab1e37d5d8d9ecba89db5e5428b0
cwangg897 <79621675+cwangg897@users.noreply.github.com>
2021-04-07 19:37:48 +0900

    Add files via upload

---
+++ Week 1/BOJ_5585번_거스름돈(최왕규).py
@@ -0,0 +1,19 @@
+
+coinList = [500,100,50,10,5,1]
+
+sum_money = 1000
+remainder = int(input(''))
+
+sum_money = sum_money - remainder
+count = 0
+
+for i in coinList:
+    while sum_money>=i:
+        sum_money = sum_money - i
+        count = count + 1
+
+print(count)
+
+        
+
+
